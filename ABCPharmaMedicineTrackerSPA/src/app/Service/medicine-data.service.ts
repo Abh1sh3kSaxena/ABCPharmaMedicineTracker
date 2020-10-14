1d6204643416c57d0acd1fe164803bf72c0f3393
Abhishek Saxena <abhisheksaxena.user@gmail.com>
2020-10-15 03:09:12 +0530

    Generic API URL

--- ABCPharmaMedicineTrackerSPA/src/app/Service/medicine-data.service.ts
+++ ABCPharmaMedicineTrackerSPA/src/app/Service/medicine-data.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Medicine, IMedicine } from '../model/medicine';
 import { MedicineDetailComponent } from '../medicine-detail/medicine-detail.component';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -13,7 +14,7 @@ export class MedicineDataService {
   constructor(private http : HttpClient) { }
 
   public async getAllMedicine() : Promise<any>{
-    return await this.http.get('http://localhost:61418/api/medicine').toPromise();
+    return await this.http.get(environment.API_URL + "api/medicine").toPromise();
   }
 
   public async getMedicineDetail() : Promise<Medicine>{
